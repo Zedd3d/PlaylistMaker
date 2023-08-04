66b737e1d49f889506ec6c935a43050209391b05
Zedd3d <yariyan@yandex.ru>
2023-08-04 18:18:58 +0300

    Third commit (9 спринт)

--- app/src/main/java/com/zeddikus/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/zeddikus/playlistmaker/SearchActivity.kt
@@ -1,11 +1,72 @@
 package com.zeddikus.playlistmaker
 
+
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
+    companion object {
+        const val SEARCH_TEXT = "SEARCH_TEXT"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        val vTextSearch = findViewById<EditText>(R.id.vTextSearch)
+        val clearButton = findViewById<ImageView>(R.id.btnSearchClear)
+        clearButton.setOnClickListener{clearSearchText(clearButton,vTextSearch)}
+        val btnBack = findViewById<ImageButton>(R.id.imgBtnBack)
+        btnBack.setOnClickListener{finish()}
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+        vTextSearch.addTextChangedListener(simpleTextWatcher)
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_TEXT,findViewById<EditText>(R.id.vTextSearch).text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        findViewById<EditText>(R.id.vTextSearch).setText(savedInstanceState.getString(SEARCH_TEXT))
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+    private fun clearSearchText(btnClose: ImageView,editField: EditText) {
+        editField.setText("")
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+        inputMethodManager?.hideSoftInputFromWindow(`editField`.windowToken, 0)
+        btnClose.visibility = View.GONE
+
+    }
+
 }
+
+
--- app/src/main/java/com/zeddikus/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/zeddikus/playlistmaker/SettingsActivity.kt
@@ -1,10 +1,14 @@
 package com.zeddikus.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageButton
+import android.widget.Toast
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : AppCompatActivity(), View.OnClickListener{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
@@ -12,5 +16,41 @@ class SettingsActivity : AppCompatActivity() {
         btnBack.setOnClickListener{
             finish()
         }
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id ) {
+                R.id.swBlackTheme -> Toast.makeText(this,"Переключение на Темную тему",Toast.LENGTH_SHORT).show()
+                R.id.llShare -> shareApp()
+                R.id.llSupport -> mailToSupport()
+                R.id.llTerms -> showTerms()
+                else -> println("Другое")
+            }
+        }
+    }
+
+    private fun showTerms() {
+        val url = Uri.parse("https://yandex.ru/legal/practicum_offer/")
+        val intent = Intent(Intent.ACTION_VIEW, url)
+        startActivity(intent)
+    }
+
+    private fun mailToSupport() {
+        val message = "Спасибо разработчикам и разработчицам за крутое приложение!"
+        val shareIntent = Intent(Intent.ACTION_SENDTO)
+        shareIntent.data = Uri.parse("mailto:")
+        shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("yariyan@yandex.ru"))
+        shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+        startActivity(shareIntent)
+    }
+
+    private fun shareApp() {
+        val sendIntent: Intent = Intent(Intent.ACTION_SEND)
+        sendIntent.putExtra(Intent.EXTRA_TEXT,"https://practicum.yandex.ru/android-developer/")
+        sendIntent.type = "text/plain"
+
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        startActivity(shareIntent)
     }
 }

4b454695183eecee5318879d3dd664f55349db12
Zedd3d <yariyan@yandex.ru>
2023-10-04 20:40:14 +0300

    13 спринт доп

--- app/src/main/java/com/zeddikus/playlistmaker/Track.kt
+++ app/src/main/java/com/zeddikus/playlistmaker/Track.kt
@@ -3,7 +3,7 @@ package com.zeddikus.playlistmaker
 import com.google.gson.annotations.SerializedName
 
 data class Track(
-    val trackName: String, // Название композиции
+    val trackName: String, // Название композиции 1
     val artistName: String, // Имя исполнителя
     @SerializedName("trackTimeMillis") val trackTime: Long, // Продолжительность трека
     val artworkUrl100: String, // Ссылка на изображение обложки
